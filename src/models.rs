833f30f3ee8aa2398ae0f7c27fbbff430476dab9
salva09 <salva.hg01@gmail.com>
2020-11-02 02:07:03 -0600

    Add new user model

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,17 @@
+use super::schema::Users;
+
 #[derive(Queryable)]
 pub struct User {
     pub id: i32,
     pub name: String,
     pub email: String,
     pub age: i32,
+}
+
+#[derive(Insertable)]
+#[table_name="Users"]
+pub struct NewUser<'a> {
+    pub name: &'a str,
+    pub email: &'a str,
+    pub age: &'a i32,
 }
