4c8619ead3501cd7336fc3f6f1c30d81afb30424
salva09 <salva.hg01@gmail.com>
2020-11-02 01:31:51 -0600

    change postgreedb stuff to sqlite

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@ use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
+pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
+    SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
